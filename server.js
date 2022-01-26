1df9dc8c46249657e5975f524258120760d5f405
trm621 <t.maher621@gmail.com>
2022-01-26 16:57:43 -0500

    tweaked server

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
   useUnifiedTopology: true
 });
 
+
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
