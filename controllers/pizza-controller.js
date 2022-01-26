978df36cf3b1ee861592134e9fb4ffcb52e81bfc
trm621 <t.maher621@gmail.com>
2022-01-26 16:08:37 -0500

    built models and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,13 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
       Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+        .select('-__v')
+        //sorts in descending order 
+        .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
           console.log(err);
@@ -14,6 +21,11 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
       Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+        .select('-__v')
         .then(dbPizzaData => {
           // If no pizza is found, send 404
           if (!dbPizzaData) {
